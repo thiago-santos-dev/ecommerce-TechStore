da0ad2faf17b51b3a55c79f092fbd3498057893d
thiago-santos-dev <thiago.fsantos@outlook.com>
2026-01-20 21:22:46 +0000

    inicio

---
+++ tech-store/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        TechStore 🛒
+      </h1>
+      <p className="text-gray-600 mt-4">Projeto com Stack de Ouro iniciando...</p>
+    </main>
+  );
+}
